5799824228c106197f76d8f9f23b89d0a144f7be
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-01-28 19:06:57 +0800

    add chat id validation

--- src/components/bot.ts
+++ src/components/bot.ts
@@ -3,13 +3,17 @@ import { WhatsApp } from "./whatsapp";
 import { EventService } from "./event/service";
 import { Message } from "whatsapp-web.js";
 
+const { BOT_CHAT_ID } = process.env;
+
 export class Bot extends WhatsApp {
     private static instance: Bot;
+    private chatId: string | number;
     private userService: UserService;
     private eventService: EventService;
 
     private constructor() {
         super();
+        this.chatId = `${BOT_CHAT_ID}`;
         this.userService = new UserService();
         this.eventService = new EventService();
     }
@@ -21,7 +25,23 @@ export class Bot extends WhatsApp {
         return Bot.instance;
     }
 
+    private validateChatId(chatId: string | number) {
+        if (!chatId) {
+            this.logger.error("chatId is required");
+            return false;
+        } else if (typeof chatId !== "string" && typeof chatId !== "number") {
+            this.logger.error("chatId must be a string or number");
+            return false;
+        } else if (`${chatId}` !== this.chatId) {
+            this.logger.error("chatId does not match bot configuration");
+            return false;
+        }
+        return true;
+    }
+
     protected async processMessage(msg: Message) {
-        this.logger.info("received whatsapp message in subclass", msg);
+        const { from: chatId } = msg;
+        if(!this.validateChatId(chatId)) return;
+        this.logger.info("received whatsapp message", msg);
     }
 }
